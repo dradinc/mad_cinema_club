c966e19924677fcfc6b12c784fb85b993304a35e
dradinc <dradinc@yandex.ru>
2024-03-13 15:07:13 +0300

    sign up | send code

--- src/api/auth/sign_up.py
+++ src/api/auth/sign_up.py
@@ -1,61 +1,59 @@
+from random import randint
+
 from flask_restful import Resource, reqparse
 from flask_json import json_response
+from flask_mail import Message
 
 from src.modules.db import UsersModel
-
-sign_up_parse = reqparse.RequestParser()
-sign_up_parse.add_argument(
-    'name',
-    type=str,
-    help='User name'
-)
-sign_up_parse.add_argument(
-    'email',
-    type=str,
-    help='User email'
-)
-sign_up_parse.add_argument(
-    'password',
-    type=str,
-    help='User password'
-)
+from src.modules.mail import app_mail
 
 
-class SignUp(Resource):
+class SigUpSendCode(Resource):
+
+    send_code = reqparse.RequestParser()
+    send_code.add_argument(
+        "email",
+        type=str,
+        help='User Email'
+    )
 
     def post(self):
         # Получаем аргументы
-        request_args = sign_up_parse.parse_args()
+        request_args = self.send_code.parse_args()
 
-        # Проверяем что все аргументы имеют значения (не пустые)
-        if not request_args['name']:
-            return json_response(
-                status_=400,
-                message=f'Аргумент \'name\' отсутствует или не содержит значений'
-            )
-        elif not request_args['email']:
-            return json_response(
-                status_=400,
-                message=f'Аргумент \'email\' отсутствует или не содержит значений'
-            )
-        elif not request_args['password']:
-            return json_response(
-                status_=400,
-                message=f'Аргумент \'password\' отсутствует или не содержит значений'
-            )
+        # Проверяем все аргументы на наличие
+        if not request_args['email']:
+            return json_response(status_=400, message='Аргумент \'email\' отсутствует или не имеет данных')
 
-        # Проверяем нет ли пользователя с указанным Email
-        if UsersModel.find_user_by_email(email=request_args['email']):
-            return json_response(
-                status_=400,
-                message='Пользователь с данным E-mail уже существует'
-            )
+        # Проверяем что пользователя с данным E-mail не существует или не закончил регистрацию
+        check_users = UsersModel.find_user_by_email(email=request_args['email'])
+        if check_users:
+            if check_users.password:
+                return json_response(
+                    status_=400,
+                    message='Пользователь с данным E-mail уже существует'
+                )
+        else:
+            # Добавляем нового пользователя
+            check_users = UsersModel(new_email=request_args['email'])
+            check_users.add_new_user()
 
-        # Создаем нового пользователя
-        new_user = UsersModel(**request_args)
-        new_user.add_new_user()
+        # Генерируем новый код
+        new_code = ''
+        for i in range(6):
+            new_code += str(randint(0, 9))
+
+        # Добавляем код пользователю
+        check_users.edit_user(**{'code': new_code})
+
+        # Отправляем уведомление на почту
+        message = Message('Код подтверждения', recipients=[request_args['email']])
+        message.html = f"""
+                    <p>Код подтверждения: %r</p>
+                """ % new_code
+        app_mail.send(message)
 
         return json_response(
             status_=200,
-            message='Пользователь успешно создан'
+            message=f'Пароль отправлен на почту \'{request_args["email"]}\''
         )
--- src/model/db/users.py
+++ src/model/db/users.py
@@ -20,19 +20,17 @@ class UsersModel(app_db.Model):
 
     # columns
     id = Column(Integer, primary_key=True)
-    name = Column(String(64), nullable=False)
+    name = Column(String(64), nullable=True)
     email = Column(String(64), nullable=False)
-    password = Column(String(512), nullable=False)
+    password = Column(String(512), nullable=True)
     balance = Column(Integer, nullable=False, default=0)
     code = Column(String(6))
     is_checker = Column(Boolean, nullable=False, default=False)
     # relationships
     tickets = relationship('TicketsModel', back_populates='user')
 
-    def __init__(self, **kwargs):
-        self.name = kwargs.get('name')
-        self.email = kwargs.get('email')
-        self.password = generate_password_hash(kwargs.get('password'))
+    def __init__(self, new_email):
+        self.email = new_email
 
     def add_new_user(self):
         """
